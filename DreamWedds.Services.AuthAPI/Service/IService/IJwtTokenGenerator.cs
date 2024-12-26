3ed55665b005b9ecd3cd011c3a786310d5478981
Neeraj <singh.neer@ymail.com>
2024-12-26 15:18:54 +0530

    AuthApi Service and Message bus

---
+++ DreamWedds.Services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using DreamWedds.Services.AuthAPI.Models;
+
+namespace DreamWedds.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles);
+    }
+}
