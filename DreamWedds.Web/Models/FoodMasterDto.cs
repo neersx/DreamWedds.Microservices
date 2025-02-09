e3b052a907be5980f9a240acf68b97cab7a26a74
Neeraj <singh.neer@ymail.com>
2025-02-09 12:11:59 +0530

    Fixed data mapping on update

--- DreamWedds.Web/Models/FoodMasterDto.cs
+++ DreamWedds.Web/Models/FoodMasterDto.cs
@@ -23,33 +23,37 @@
         public bool? AvailableToday { get; set; }
         public double? Ratings { get; set; }
 
+        public List<IngredientDto> Ingredients { get; set; }
+        public List<Image> Images { get; set; }
+        public List<FoodItem> FoodItems { get; set; }
+
     }
 
     public class IngredientDto
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string HealthBenefits { get; set; }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? HealthBenefits { get; set; }
     }
 
     public class Image
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Title { get; set; }
-        public string Url { get; set; }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Title { get; set; }
+        public string? Url { get; set; }
     }
 
     public class FoodItem
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public bool IsRequired { get; set; }
-        public bool AvailableToday { get; set; }
-        public string Weight { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public int? Quantity { get; set; }
+        public bool? IsRequired { get; set; }
+        public bool? AvailableToday { get; set; }
+        public string? Weight { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
     }
 
 }
