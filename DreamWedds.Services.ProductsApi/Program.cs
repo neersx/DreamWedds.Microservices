86598fd84ebba444705ffb7fa5137f1dfa346507
Neeraj <singh.neer@ymail.com>
2025-04-05 22:58:32 +0530

    Public Prod Swagger

--- DreamWedds.Services.ProductsApi/Program.cs
+++ DreamWedds.Services.ProductsApi/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddScoped<IIngredientsRepository, IngredientRepository>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Food Menu Services", Version = "v1" });
@@ -43,12 +44,15 @@ var app = builder.Build();
 
 app.UseCors("AllowSpecificOrigins");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product Service v1");
+    c.RoutePrefix = "swagger"; // or "" if you want Swagger at "/"
+});
+
 
 app.UseHttpsRedirection();
 
